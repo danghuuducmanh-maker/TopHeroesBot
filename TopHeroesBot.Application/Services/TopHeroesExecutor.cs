ee347e696877436747f2b6aac5b2463f717ba0b8
Đức Mạnh <danghuuducmanh@gmail.com>
2026-08-07 15:23:47 +0700

    refactor: move login workflow to TopHeroesExecutor

--- TopHeroesBot.Application/Services/TopHeroesExecutor.cs
+++ TopHeroesBot.Application/Services/TopHeroesExecutor.cs
@@ -8,11 +8,38 @@ public class TopHeroesExecutor : ITopHeroesExecutor
 {
     private readonly ITopHeroesClient _topHeroesClient;
 
-    public Task<NotifyContext?> CreateContextAsync(
-        string uid,
-        Func<string, Task>? notify = null)
+    public async Task<NotifyContext?> CreateContextAsync(
+    string uid,
+    Func<string, Task>? notify = null)
     {
-        throw new NotImplementedException();
+        await _topHeroesClient.CreatePageAsync();
+
+        try
+        {
+            await _topHeroesClient.LoginAsync(uid);
+
+            var profile = await _topHeroesClient.GetPlayerProfileAsync();
+
+            var context = new NotifyContext
+            {
+                Uid = uid,
+                Profile = profile,
+                Notify = notify
+            };
+
+            if (notify != null)
+            {
+                await notify(
+                    $"[{DateTime.Now:HH:mm:ss}] {profile.Name} ({profile.Server}): Đăng nhập thành công.");
+            }
+
+            return context;
+        }
+        catch
+        {
+            await _topHeroesClient.CloseAsync();
+            return null;
+        }
     }
 
     public Task ExecuteAsync(
